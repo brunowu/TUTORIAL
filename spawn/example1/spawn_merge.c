bdf6cac497fffc62145f20c26b0ee4e13b0b6d10
brunowu <xinzhe.wu1990@gmail.com>
2017-06-14 15:40:54 +0200

    worker-slave

--- spawn/example1/spawn_merge.c
+++ spawn/example1/spawn_merge.c
@@ -1,4 +1,4 @@
-/*mpicc -o spawnMerge spawnMerge.c
+/*mpicc -o spawnMerge spawn_merge.c
  *mpirun -np 1 spawnMerge
 */
 #include <stdio.h>
@@ -21,14 +21,14 @@ int main(int argc, char **argv)
 
             MPI_Comm_size(MPI_COMM_WORLD,&w_size);
             MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
-            printf("Slave task started, world size=%d my rank=%d\n",w_size,my_rank);
+            printf("----Slave task started, world size=%d my rank=%d\n",w_size,my_rank);
 
-            printf("Slave task, merging the communicator...\n");
+            printf("----Slave task, merging the communicator...\n");
             MPI_Intercomm_merge(boss_comm,1,&comm);
 
             MPI_Comm_size(comm,&w_size);
             MPI_Comm_rank(comm,&my_rank);
-            printf("Slave task after the merge, world size=%d my rank=%d\n",w_size,my_rank);
+            printf("----Slave task after the merge, world size=%d my rank=%d\n",w_size,my_rank);
 
 
         } else {
@@ -38,19 +38,19 @@ int main(int argc, char **argv)
             MPI_Comm intercomm_1,intercomm_2,comm_1,comm_2,full_comm1,full_comm2;
             MPI_Comm intercomm_full;
 
-            printf("Master task: first spawn\n");
+            printf("]> Master task: first spawn\n");
             MPI_Comm_dup(MPI_COMM_SELF,&comm_1);
             MPI_Comm_spawn("spawnMerge",args,2,MPI_INFO_NULL,0,comm_1,&intercomm_1,(int*)MPI_ERRCODES_IGNORE);
 
-            printf("Master task: second spawn\n");
+            printf("]> Master task: second spawn\n");
             MPI_Comm_dup(MPI_COMM_SELF,&comm_2);
             MPI_Comm_spawn("spawnMerge",args,2,MPI_INFO_NULL,0,comm_2,&intercomm_2,(int*)MPI_ERRCODES_IGNORE);
 
-            printf("Master task: the two groups have been spawned\n");
-            printf("Master task: merging the first communicator...\n");
+            printf("]> Master task: the two groups have been spawned\n");
+            printf("]> Master task: merging the first communicator...\n");
             MPI_Intercomm_merge(intercomm_1,0,&full_comm1);
 
-            printf("Master task: merging the second communicator...\n");
+            printf("]> Master task: merging the second communicator...\n");
             MPI_Intercomm_merge(intercomm_2,0,&full_comm2);
         }
 
