c49b716d21457f57916e2e6c406875ec34fd504c
brunowu <xinzhe.wu1990@gmail.com>
2017-08-21 12:25:36 +0200

    spawn+merge communication

---
+++ spawn/example6/slave2.c
@@ -0,0 +1,43 @@
+#include <stdio.h> 
+#include <mpi.h>
+
+int main( int argc, char *argv[] ) {
+
+  MPI_Init( &argc, &argv );
+
+  int myrank;
+  MPI_Comm_rank( MPI_COMM_WORLD, &myrank );
+
+  MPI_Comm parentcomm;
+  MPI_Comm_get_parent( &parentcomm );
+
+  MPI_Comm intra_comm2;
+  MPI_Intercomm_merge( parentcomm, 1, &intra_comm2 );
+
+  if ( myrank == 0 ) {
+    int tmp_size;
+    MPI_Comm_remote_size( parentcomm, &tmp_size );
+    printf( "child size of parent comm world = %d\n", tmp_size );
+
+    MPI_Comm_size( MPI_COMM_WORLD, &tmp_size );
+    printf( "child size of child comm2 world = %d\n", tmp_size );
+
+    MPI_Comm_size( intra_comm2, &tmp_size );
+    printf( "child size of intra comm2 world = %d\n", tmp_size );
+  }
+  //MPI_Barrier(MPI_COMM_WORLD);
+  int intra_rank2;
+
+  MPI_Comm_rank(intra_comm2, &intra_rank2);
+  printf("I am rank %d in intra_rank2 \n", intra_rank2);
+  int msg;
+  MPI_Status status;
+  MPI_Recv(&msg,1, MPI_INT, 0,0,intra_comm2,&status);
+  printf("\n###Child 2 recv msg = %d from parent \n", msg);
+//  MPI_Send(&msg, 1, MPI_INT, 0,0,intra_comm);
+  MPI_Comm_free(&parentcomm);
+  MPI_Comm_free(&intra_comm2);
+  MPI_Finalize();
+
+  return 0;
+}
