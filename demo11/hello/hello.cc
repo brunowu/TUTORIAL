2d40554e3425f7a29245f13839d48386ee70f7d7
brunowu <xinzhe.wu1990@gmail.com>
2018-03-27 16:00:28 +0200

    cmake MPICXX Basic

---
+++ demo11/hello/hello.cc
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+void hello()
+{
+	std::cout << "hello from hello function" << std::endl; 
+}
---
+++ demo11/world/world.cc
@@ -0,0 +1,6 @@
+#include "world.h"
+
+void world()
+{
+	std::cout << "world from world function" << std::endl; 
+}
